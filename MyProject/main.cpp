c1e463c2bc0769c0ec7d8a28b336184d345560cf
Miroslav262 <mirostavchik235@gmail.com>
2024-11-27 16:53:55 +0300

    Реализовано чтение данных из файла внесены извенения в main

--- MyProject/main.cpp
+++ MyProject/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <Windows.h>
+#include <locale.h>
+#include "constants.h"
+#include "file_reader.h"
+#include "telephone_info.h"
 using namespace std;
 
 int main()
 {
+    setlocale(LC_CTYPE, "Russian");
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #9. Library Subscription\n";
     cout << "Author: Markushevski Miraslau\n";
-
+    info* information[MAX_FILE_ROWS_COUNT];
+    int size;
+    try
+    {
+        reading(information, "data.txt", size);
+        for (int i = 0; i < size; i++)
+        {
+            cout << information[i]->number << '\n';
+            cout << information[i]->date.day << " " << information[i]->date.month << " " << information[i]->date.year << '\n';
+            cout << information[i]->time.hours << " " << information[i]->time.minutes << " " << information[i]->time.seconds << '\n';
+            cout << information[i]->lasting.hours << " " << information[i]->lasting.minutes << " " << information[i]->lasting.seconds << '\n';
+            cout << information[i]->rate << '\n';
+            cout << information[i]->cost << '\n';
+            cout << '\n';
+        }
+        for (int i = 0; i < size; i++)
+        {
+            delete information[i];
+        }
+    }
+    catch (const char* error)
+    {
+        cout << error << '\n';
+    }
+    return 0;
+    system("pause");
 }
